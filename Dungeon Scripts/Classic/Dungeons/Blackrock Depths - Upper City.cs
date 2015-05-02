8aeed4926c1cae916858d642a181e605c7bbaf51
Exciter101 <Exciter101@users.noreply.github.com>
2015-05-02 09:22:11 +0200

    Vorbereitung auf Version 786 EU

--- Dungeon Scripts/Classic/Dungeons/Blackrock Depths - Upper City.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Bots.DungeonBuddy.Dungeon_Scripts.Classic
-{
-	public class BlackrockDepthsUpperCity : BlackrockDepthsDetentionBlock
-	{
-		public override uint DungeonId { get { return 276; } }
-	}
-}
